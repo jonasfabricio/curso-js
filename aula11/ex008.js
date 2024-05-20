e6b4ab5d6f1f18478604d3e591a223b589f6fc17
jonasfabricio <167653009+jonasfabricio@users.noreply.github.com>
2024-05-20 19:33:20 -0300

    novos exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var peso = 74.0
+console.log(`Seu peso atula é de ${peso}Kg.`)
+if(peso >= 75.0){
+    console.log('Você está acima da média')
+} //condição simples
+console.log('Obrigado pela consulta.')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Seu país é ${país}.`)
+if (país == 'BR'){
+    console.log('Você é brasileiro.')
+}else{
+    console.log("Você é estrangeiro.")
+} //condição composta
