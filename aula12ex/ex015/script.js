0f98549792b865f90858e481b9c979a44899fba0
jonasfabricio <167653009+jonasfabricio@users.noreply.github.com>
2024-05-23 08:27:45 -0300

    exercicios completos

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,22 @@
-var anonasc = document.getElementById('anonasc')
+function verificar() {
+    var anonasctxt = document.getElementById('anonasc')
+    var anonasc = Number(anonasctxt.value)
+    var anoatual = Number(2024)
+    if (anonasc == 0 || anonasc > anoatual){
+        window.alert('[ERROR] Verifique os dados e tente novamente.')
+    }else {    
+    var idade = anoatual - anonasc
+    var resultado = document.getElementById('res')
+    var sex = document.getElementsByName('radsex')
+    var genero = ''
+    if (sex[0].checked){
+        genero = 'Homem'
+    }else if (sex[1].checked){
+        genero = 'Mulher'
+    }else{
+        window.alert('[ERROR] Selecione seu gênero e tente novamente.')
+    }    
+    resultado.innerHTML = `Identificamos que você é ${genero}, e tem ${idade} anos.`
+    }
+
+}
