2f6ae09f6e69d9a2ea4f230b5641cd31b7ec0185
jonasfabricio <167653009+jonasfabricio@users.noreply.github.com>
2024-06-01 12:11:18 -0300

    acabou o curso de JS no Curso-em-video.

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,17 +1,61 @@
-function adicionar(){
-    let txtn = document.getElementById('txtn')
-    let res = document.getElementById('select')
-    let n = Number(txtn.value)
+let numero = document.getElementById('txtn')
+let tabela = document.getElementById('select')
+let resultado = document.getElementById('res')
+let lista = []
 
-    if(n > 0 && n <= 100){
-        let item = document.createElement('option')
-        item.text = (`O valor ${n} foi adicionado.`)
-        res.appendChild(item)
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     } else {
-        window.alert('[ERROR] Verfique se o número atende aos requisitos.')
+        return false
     }
 }
 
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(numero.value) && !inLista(numero.value, lista)){
+        lista.push(Number(numero.value))
+        let item = document.createElement('option')
+        item.text = `O valor ${numero.value} foi adicionado.`
+        tabela.appendChild(item)
+    } else {
+        window.alert('[ERROR] Verifique o número e tente novamente!')
+    }
+    numero.value = ''
+    numero.focus()
+    resultado.innerHTML = ''
+}
+
 function finalizar(){
-    // Me perdi na hora de ter que armazenar os "n" em uma lista.
+    let total = lista.length
+    let menor = lista[0]
+    let maior = lista[0]
+    let soma = 0
+    let media = 0
+
+    if(lista.length == 0){
+        window.alert('[ERROR] Adicione números a lista antes de finalizar.')
+    } else {
+        for (let pos in lista){
+            soma += lista[pos]
+            if(lista[pos] > maior)
+                maior = lista[pos]
+            if(lista[pos] < menor)
+                menor = lista[pos]
+        }
+        media = soma / total
+        resultado.innerText = ''
+        resultado.innerHTML += `Ao todo temos ${total} números. <br>`
+        resultado.innerHTML += `O menor valor informado foi ${menor}. <br>`
+        resultado.innerHTML += `O maior valor informado foi ${maior}. <br>`
+        resultado.innerHTML += `A soma de todos os números é ${soma}. <br>`
+        resultado.innerHTML += `A média dos números é ${media}. <br>`
+    }
 }
---
+++ aula17/object1.js
@@ -0,0 +1,4 @@
+let amigo = {nome:'José', sexo:'M', peso:60, engordar(p){
+ 
+}}
+console.log(`${amigo.nome} pesa ${amigo.peso}.`)
